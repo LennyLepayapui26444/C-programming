494030c27d86a42cfa0f730139fc031ddcba0945
LennyLepayapui26444 <lennylepayapui660@gmail.com>
2025-09-23 10:24:27 +0300

    Add files via upload

---
+++ Semester1Units (2).c
@@ -0,0 +1,22 @@
+/*
+Name:LennyLepayapui 
+Reg NO: CT101/G/26444/25
+Description: Program to units of this semester 
+*/
+#include<stdio.h>
+
+int main() {
+
+printf("UNITS THIS SEMESTER\n");
+
+printf("1. Introduction to programming and problem solving\n");
+printf("2. Introduction to computer architecture\n");
+printf("3. Physics\n");
+printf("4. Analogue electronics\n");
+printf("5. Discrete mathematics\n");
+printf("6. Mathematics for science\n");
+printf("7. Communication skills\n");
+printf("8. Fundamental of ICT");
+
+return 0;
+}
