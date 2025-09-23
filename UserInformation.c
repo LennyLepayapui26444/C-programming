2707bc7bb3828df5eac94d95dce0b9d839a61ee6
LennyLepayapui26444 <lennylepayapui660@gmail.com>
2025-09-23 10:58:59 +0300

    Add files via upload

---
+++ UserInformation.c
@@ -0,0 +1,23 @@
+/*
+Name:Lenny Lepayapui 
+Reg NO:CT101/G/26444/25
+Description:C program to prompt the user to fill their information 
+*/
+
+#include <stdio.h>
+int main(){
+
+int Height ;
+printf("Your height:");
+scanf("%d",&Height);
+
+int Bankbalance;
+printf("\nYour bank balance:");
+scanf("%d",&Bankbalance);
+
+int Phonenumber;
+printf("\nYour phone number:");
+scanf("%d",&Phonenumber);
+
+return 0;
+}
