5bfc766c7b63044f92a9161c1d3f4728acc68ce4
LennyLepayapui26444 <lennylepayapui660@gmail.com>
2025-10-09 12:20:51 +0300

    Add files via upload

---
+++ DescendingNumbers.c
@@ -0,0 +1,16 @@
+/*
+Name:Lenny Lepayapui
+Reg NO:CT101/G/26444/25
+Description C program to print numbers descending from 100 to 50
+Date:8/10/2025
+*/
+#include <stdio.h>
+int main() {
+
+    int i;
+    for(i=100; i>=50; i--) {
+        printf("%d\n",i);
+    }
+
+    return 0;
+}
